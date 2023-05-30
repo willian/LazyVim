bc1434f25298aa18b2b288839cb556a193f2cbeb
Willian Fernandes <willian@willianfernandes.com.br>
2023-05-30 09:12:03 -0300

    Auto close neo-tree when opening a file

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -133,4 +133,19 @@ return {
     },
     config = true,
   },
+
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    opts = {
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            -- auto close
+            require('neo-tree').close_all()
+          end,
+        },
+      },
+    },
+  },
 }
