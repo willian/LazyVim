a5206137af03c4422a43ce6934c9100833df1506
Willian Fernandes <willian@willianfernandes.com.br>
2023-07-03 15:34:33 -0300

    Install neotest with rspec, minitest, jest, and vitest adapters

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.ui.mini-animate' },
     { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.formatting.prettier' },
+    { import = 'lazyvim.plugins.extras.test.core' },
+    { import = 'lazyvim.plugins.extras.dap.core' },
     -- import/override with your plugins
     { import = 'plugins' },
   },
---
+++ lua/plugins/test.lua
@@ -0,0 +1,34 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'haydenmeade/neotest-jest',
+    'marilari88/neotest-vitest',
+    'olimorris/neotest-rspec',
+    'zidhuss/neotest-minitest',
+  },
+  keys = {
+    {
+      '<leader>tl',
+      function()
+        require('neotest').run.run_last()
+      end,
+      desc = 'Run Last Test',
+    },
+  },
+  opts = function(_, opts)
+    table.insert(
+      opts.adapters,
+      require('neotest-jest')({
+        jestCommand = 'npm test --',
+        jestConfig = 'jest.config.js',
+        env = { CI = true },
+        cwd = function()
+          return vim.fn.getcwd()
+        end,
+      })
+    )
+    table.insert(opts.adapters, require('neotest-vitest'))
+    table.insert(opts.adapters, require('neotest-rspec'))
+    table.insert(opts.adapters, require('neotest-minitest'))
+  end,
+}
